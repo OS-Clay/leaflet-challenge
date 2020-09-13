1eb5a0f8c527efdcc05de504a8cbc50cfa120321
OS-Clay <64096411+OS-Clay@users.noreply.github.com>
2020-09-13 11:27:31 -0400

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,96 +1,88 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom:5
 });
 
-function createFeatures(earthquakeData) {
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken:'pk.eyJ1IjoiY2hyaWJhciIsImEiOiJja2YwYnNxODgxYnhnMndtZmplN3JocTM1In0.Y5rrDiwGiMkVyu0f4p2RHA'
+    }).addTo(myMap);
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
+
+  var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+
+  d3.json(queryUrl, function(data){
+    function circleRender(feature){
+    return {
+      opacity: .75,
+      fillOpacity: .75,
+      fillColor: Color(feature.properties.mag),
+      color:"white",
+      radius: Radius(feature.properties.mag)
+    };
+
+
+  }
+
+  function Color(m){
+  /*  colorNumeric("BuPu", c(2.5, 6), na.color = "#808080", alpha = FALSE,
+  reverse = FALSE)
+*/
+  switch (true){
+  case m > 6:
+    return "#FF6347";
+  case m > 4.5:
+    return "#FF4500";
+  case m > 3:
+    return "#FFD700";
+  case m > 2.5:
+    return "#FFA500";
+  case m > 1.5:
+    return "green"
+    }
+
+  }
+
+  function Radius(m){
+
+
+  return (m * 3)
+  }
+
+  L.geoJson(data, {
+// fun fact, circleMarker is different than circle. circle doesnt respect radius
+  pointToLayer: function(feature, coord){
+    return L.circleMarker(coord);
+  },
+  style: circleRender,
+  onEachFeature: function(feature, layer){
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p>" + feature.properties.mag + "</p>");
   }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  });
-
-  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.dark",
-    accessToken: API_KEY
-  });
-
-  /* function getColor(d) {
-    return d > 6 ? '#800026' :
-           d > 5  ? '#BD0026' :
-           d > 4  ? '#E31A1C' :
-           d > 3.5  ? '#FC4E2A' :
-           d > 3   ? '#FD8D3C' :
-           d > 2.7  ? '#FEB24C' :
-           d > 2.5   ? '#FED976' :
-                      '#FFEDA0';
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.mag),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-
-L.geoJson(earthquakeData, {style: style}).addTo(map)
- */
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
   }).addTo(myMap);
-}
+// so there is a legend, but i dont know how to get the backround colors
+//or the backround itslef
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [1.5, 2.5, 3, 4.5],
+      labels = [];
+
+      
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + Color(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+  };
+
+  legend.addTo(myMap);
+});
